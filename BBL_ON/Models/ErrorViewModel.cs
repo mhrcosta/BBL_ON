73e5e1309de2eed16ccaeda70110d9e428093750
Maria Costa <estt13767@ipt.pt>
2018-07-01 15:44:39 +0100

    test

---
+++ BBL_ON/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BBL_ON.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
